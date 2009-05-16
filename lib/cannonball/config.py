f42edd190f52029e3329ee14a5c9b244641f5712
Mikael Lind <elemel@elemel.se>
2009-05-16 08:25:51 +0200

    handle environment variable CANNONBALL_ROOT

--- lib/cannonball/config.py
+++ lib/cannonball/config.py
@@ -3,6 +3,7 @@ import os
 root = os.path.abspath(__file__)
 for _ in xrange(3):
     root = os.path.dirname(root)
+root = os.environ.get('CANNONBALL_ROOT', root)
 
 cannonball_max_angular_velocity = 15
 cannonball_max_angular_acceleration = 10
