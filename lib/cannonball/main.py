1116a855626d0f21b3b7551e5565a7a152594a00
Mikael Lind <elemel@elemel.se>
2009-05-22 15:53:09 +0200

    adjusted lights again

--- lib/cannonball/main.py
+++ lib/cannonball/main.py
@@ -35,13 +35,14 @@ class CannonballWindow(pyglet.window.Window):
         # Set up key light.
         glEnable(GL_LIGHT0)
         glLightfv(GL_LIGHT0, GL_POSITION, Float4(-1, 1, 1, 0))
-        glLightfv(GL_LIGHT0, GL_AMBIENT, Float3(0.5, 0.5, 0.5))
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, Float3(0.3, 0.3, 0.3))
+        glLightfv(GL_LIGHT0, GL_AMBIENT, Float3(0.4, 0.4, 0.4))
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, Float3(0.6, 0.6, 0.6))
 
         # Set up the fill light.
         glEnable(GL_LIGHT1)
         glLightfv(GL_LIGHT1, GL_POSITION, Float4(1, 0, 1, 0))
-        glLightfv(GL_LIGHT1, GL_DIFFUSE, Float3(0.1, 0.1, 0.1))
+        glLightfv(GL_LIGHT0, GL_AMBIENT, Float3(0.2, 0.2, 0.2))
+        glLightfv(GL_LIGHT1, GL_DIFFUSE, Float3(0.3, 0.3, 0.3))
 
         # Set up the rim light.
         # glEnable(GL_LIGHT2)
